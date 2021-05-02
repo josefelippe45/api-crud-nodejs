4c1575523bd17579734b263601a3a5ab943ae9f8
José Felippe Gonçalves Oliveira <jose.oliveira370@etec.sp.gov.br>
2021-05-01 21:34:20 -0300

    feat: add model and post route to users

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,29 @@
 const router = require("express").Router();
+const User = require("../models/user");
 
 router.get("/", (req, res, next) => {
   res.status(200).json({ msg: "working..." });
+});
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name, password } = req.body;
+  const user = new User({
+    name,
+    password,
+  });
+  try {
+    const newUser = await user.save();
+    res.status(201).json({ newUser });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 module.exports = router;
